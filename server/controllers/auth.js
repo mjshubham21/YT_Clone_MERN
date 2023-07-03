5967a3eff53684ffa49ff50029fbd99f67945c4e
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-07-03 21:00:44 +0530

    Added signin route and controller.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import User from "../models/user.js";
 import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const salt = bcrypt.genSaltSync(10);
@@ -14,5 +15,27 @@ export const signup = async (req, res, next) => {
     next(err);
   }
 };
-export const signin = async (req, res) => {};
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name }); // find user by name
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+    const isCorrect = await bcrypt.compare(req.body.password, user.password); // compare password
+    if (!isCorrect) {
+      return createError(400, "Wrong password");
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password, ...info } = user._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(info);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// export const signin = async (req, res) => {};
 export const googleLogin = async (req, res) => {};
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/users.js";
 import commentRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -30,6 +31,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
