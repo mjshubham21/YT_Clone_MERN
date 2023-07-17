1e53c20e77241da68887e9ada487b37f699e5539
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-07-17 18:58:17 +0530

    Added Subscribe button.

--- client/YT_Clone/src/components/Menu.jsx
+++ client/YT_Clone/src/components/Menu.jsx
@@ -14,6 +14,8 @@ import {
   Flag as FlagIcon,
   HelpOutline as HelpOutlineIcon,
   LightModeOutlined as LightModeOutlinedIcon,
+  AccountCircleOutlined as AccountCircleOutlinedIcon,
+  AccountCircleOutlined,
 } from "@mui/icons-material";
 
 const Container = styled.div`
@@ -43,8 +45,37 @@ const Item = styled.div`
   display: flex;
   align-items: center;
   gap: 0.5rem;
-  margin-bottom: 0.5rem;
+  ${"" /* margin-bottom: 0.5rem; */}
   cursor: pointer;
+`;
+
+const Hr = styled.hr`
+  margin: 1rem 0;
+  border: 0.5px solid #373737;
+`;
+
+//Login
+const Login = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  line-height: 1.5;
+  font-size: 0.75rem;
+`;
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  border-radius: 0.25rem;
+  color: #3ea6ff;
+  cursor: pointer;
+  margin-top: 0.2rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 0.875rem;
+  font-weight: bold;
 `;
 
 function Menu() {
@@ -70,6 +101,15 @@ function Menu() {
           <SubscriptionsIcon />
           Subscriptions
         </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <Button>
+            <AccountCircleOutlined />
+            SIGN IN
+          </Button>
+        </Login>
+        <Hr />
         <Item>
           {" "}
           <VideoLibraryIcon />
@@ -80,6 +120,7 @@ function Menu() {
           <HistoryIcon />
           History
         </Item>
+        <Hr />
         <Item>
           {" "}
           <LibraryMusicIcon />
@@ -110,6 +151,7 @@ function Menu() {
           <HistoryIcon />
           Live
         </Item>
+        <Hr />
         <Item>
           {" "}
           <LibraryMusicIcon />
