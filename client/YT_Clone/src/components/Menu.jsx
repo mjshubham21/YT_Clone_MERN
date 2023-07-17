724a79b1b75b58c38490165fd8942fe85ee84210
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-07-17 18:38:35 +0530

    Added all the MUI logos, gotta figure out about the import...

--- client/YT_Clone/src/components/Menu.jsx
+++ client/YT_Clone/src/components/Menu.jsx
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 import logo from "../img/logo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import HistoryIcon from "@mui/icons-material/History";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import MovieCreationIcon from "@mui/icons-material/MovieCreation";
+import FlagIcon from "@mui/icons-material/Flag";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 
 const Container = styled.div`
   flex: 1;
@@ -15,9 +27,9 @@ const Wrapper = styled.div`
 const Logo = styled.div`
   display: flex;
   align-items: center;
-  gap: 0.5rem;
+  gap: 0.5em;
   font-weight: bold;
-  margin-bottom: 1.5rem;
+  margin-bottom: 0.5rem;
 `;
 
 const Img = styled.img`
@@ -28,7 +40,7 @@ const Item = styled.div`
   display: flex;
   align-items: center;
   gap: 0.5rem;
-  margin-bottom: 1.5rem;
+  margin-bottom: 0.5rem;
   cursor: pointer;
 `;
 
@@ -40,7 +52,81 @@ function Menu() {
           <Img src={logo} alt="logo" />
           MJtube
         </Logo>
-        <Item>Home.</Item>
+        <Item>
+          {" "}
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          {" "}
+          <ExploreIcon />
+          Explore
+        </Item>
+        <Item>
+          {" "}
+          <SubscriptionsIcon />
+          Subscriptions
+        </Item>
+        <Item>
+          {" "}
+          <VideoLibraryIcon />
+          Library
+        </Item>
+        <Item>
+          {" "}
+          <HistoryIcon />
+          History
+        </Item>
+        <Item>
+          {" "}
+          <LibraryMusicIcon />
+          Music
+        </Item>
+        <Item>
+          {" "}
+          <SportsBasketballIcon />
+          Sports
+        </Item>
+        <Item>
+          {" "}
+          <SportsEsportsIcon />
+          Gaming
+        </Item>
+        <Item>
+          {" "}
+          <MovieCreationIcon />
+          Movies
+        </Item>
+        <Item>
+          {" "}
+          <VideoLibraryIcon />
+          News
+        </Item>
+        <Item>
+          {" "}
+          <HistoryIcon />
+          Live
+        </Item>
+        <Item>
+          {" "}
+          <LibraryMusicIcon />
+          Settings
+        </Item>
+        <Item>
+          {" "}
+          <FlagIcon />
+          Report
+        </Item>
+        <Item>
+          {" "}
+          <HelpOutlineIcon />
+          Help
+        </Item>
+        <Item>
+          {" "}
+          <LightModeOutlinedIcon />
+          Light Mode
+        </Item>
       </Wrapper>
     </Container>
   );
