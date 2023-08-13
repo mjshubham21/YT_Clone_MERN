b03783056cfc48e41b15b7f2d205fc64939b0ce7
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-08-13 18:18:02 +0530

    Finished the client side for now, gonna connect it to the backend now...

--- client/YT_Clone/src/pages/Signin.jsx
+++ client/YT_Clone/src/pages/Signin.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: calc(100dvh - 3.5rem);
@@ -20,35 +21,102 @@ const Wrapper = styled.div`
   gap: 0.625rem;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`;
 
-const SubTitle = styled.h2``;
+const SubTitle = styled.h2`
+  font-size: 1rem;
+  font-weight: 300;
+  color: ${({ theme }) => theme.textSoft};
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  width: 100%;
+  padding: 0.5rem 0.75rem;
+  border: 1px solid ${({ theme }) => theme.soft};
+  border-radius: 0.25rem;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text};
+  font-size: 0.875rem;
+  &::placeholder {
+    color: ${({ theme }) => theme.textSoft};
+  }
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 100%;
+  padding: 0.5rem 0.75rem;
+  border: none;
+  font-weight: 500;
+  border-radius: 0.25rem;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+  font-size: 0.875rem;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+  &:hover {
+    background-color: ${({ theme }) => theme.bgLighter};
+  }
+`;
 
-const More = styled.div``;
+const More = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  font-size: 0.875rem;
+  color: ${({ theme }) => theme.textSoft};
+  margin-top: 0.5rem;
+  padding-top: 0.5rem;
+  border-top: 1px solid ${({ theme }) => theme.soft};
+`;
+
+const Links = styled.div`
+  margin-left: 3.125rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const Link = styled.span`
+  color: ${({ theme }) => theme.text};
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 function SignIn() {
   return (
     <Container>
       <Wrapper>
         {/* SignIn */}
-        <Title>SignIn</Title>
+        <Title>Sign-In</Title>
         <SubTitle>to continue to MJYouTube</SubTitle>
         <Input type="text" placeholder="Email or phone" />
         <Input type="password" placeholder="Password" />
-        <Button>Sign In</Button>
+        <Button>Sign-In</Button>
 
         <SubTitle>OR</SubTitle>
 
         {/* SignUp */}
-        <Title>SignUp</Title>
-        <Input type="text" placeholder="Email or phone" />
+        <Title>Sign-Up</Title>
+        <Input type="text" placeholder="Name" />
+        <Input type="text" placeholder="Email or Phone." />
         <Input type="password" placeholder="Password" />
-        <Button>Sign Up</Button>
-        <More>More options</More>
+        <Button>Sign-Up</Button>
+        <More>
+          English(USA)
+          <Links>
+            <Link>Help</Link>
+            <Link>Privacy</Link>
+            <Link>Terms</Link>
+          </Links>
+        </More>
       </Wrapper>
     </Container>
   );
