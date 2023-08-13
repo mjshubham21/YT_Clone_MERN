32da96d4bf07bcc24c352fbc8644bfe74a4bd511
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-08-13 18:26:28 +0530

    Trying to connect front and backend.

--- client/YT_Clone/src/pages/Home.jsx
+++ client/YT_Clone/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { styled } from "styled-components";
 import Card from "../components/Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Container = styled.div`
   display: flex;
@@ -9,28 +11,28 @@ const Container = styled.div`
 `;
 
 function Home() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get("/videos/random");
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, []);
+
   return (
     <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {videos.map((video) => (
+        <Card
+          key={video._id}
+          title={video.title}
+          thumbnail={video.thumbnail}
+          channel={video.channel}
+          views={video.views}
+          createdAt={video.createdAt}
+        />
+      ))}
     </Container>
   );
 }
